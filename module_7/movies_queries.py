fbbf36a8098785165579ec09fa1050fb6d38a1be
RachRoxIt <111474552+RachRoxIt@users.noreply.github.com>
2022-11-29 22:30:52 -0600

    Update movies_queries.py

--- module_7/movies_queries.py
+++ module_7/movies_queries.py
@@ -35,40 +35,29 @@ except mysql.connector.Error as err:
     else:
         print(err)
 
-print(" -- DISPLAYING Studio RECORDS -- ")
-cursor = db.cursor()
-StudioQuery = "SELECT studio_id, studio_name FROM studio"
-cursor.execute(StudioQuery)
-studios = cursor.fetchall()
-for studio in studios:
-	print("Studio ID: {}\nStudio Name: {}\n".format(studio[0], studio[1]))
-
-
 print(" -- DISPLAYING Genre RECORDS -- ")
 cursor = db.cursor()
 GenreQuery = "SELECT genre_id, genre_name FROM genre"
 cursor.execute(GenreQuery)
 genres = cursor.fetchall()
 for genre in genres:
-	print("Genre ID: {}\nGenre Name: {}\n".format(genre[0], genre[1]))
-
+    print("Genre ID: {}\nGenre Name: {}\n".format(genre[0], genre[1]))
 
 print(" -- DISPLAYING Short Film RECORDS -- ")
 cursor = db.cursor()
-SFilmQuery = "SELECT film_name, film_runtime FROM film"
+SFilmQuery = "SELECT film_name, film_runtime FROM film WHERE film_runtime < 120"
 cursor.execute(SFilmQuery)
 films = cursor.fetchall()
 for film in films:
-	print("Film Name: {}\nRuntime: {}\n".format(film[0], film[1]))
-
+    print("Film Name: {}\nRuntime: {}\n".format(film[0], film[1]))
 
 print(" -- DISPLAYING Director RECORDS -- ")
 cursor = db.cursor()
-DirectorQuery = "SELECT genre_id, genre_name FROM genre"
+DirectorQuery = "SELECT film_name, film_director FROM film ORDER BY film_director;"
 cursor.execute(DirectorQuery)
 directors = cursor.fetchall()
 for director in directors:
-	print("Film Name: {}\nDirector: {}\n".format(director[0], director[1]))
+    print("Film Name: {}\nDirector: {}\n".format(director[0], director[1]))
 
 
 db.close()
